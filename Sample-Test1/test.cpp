43c18e0379da1dabfd0f209e20ff3d3148793ff5
ck412.park <pigun412@gmail.com>
2023-07-07 10:15:12 +0900

    First Commit

---
+++ Sample-Test1/test.cpp
@@ -0,0 +1,12 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <vector>
+#include "../230707_CRA_DeviceDriver/DeviceDirver.cpp"
+
+using namespace testing;
+using namespace std;
+
+TEST(TestCaseName, testName) {
+
+	EXPECT_THAT(1, Eq(1));
+}
